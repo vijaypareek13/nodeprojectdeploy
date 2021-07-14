79cb6581d78b27ff19631f1a375400b49ab51d41
unknown <vijay.pareek13@gmail.com>
2021-07-14 17:37:00 +0530

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!')
+})
+app.get('/about', (req, res) => {
+  res.send('about World!!')
 })
 
 app.listen(port, () => {
